3a2d3105d93939d253d3e2d22c9d739cc771c0a6
mishasro72 <mishasro72@gmail.com>
2024-07-16 18:48:00 -0400

    Add test_main_menu

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -6,6 +6,15 @@ class DashboardPage():
     def __init__(self, page: Page):
         self.page = page
         self.welc_messege = page.locator("#usernameDisplay")
+        self.main_menu = page.locator('//div[@class="list-group list-group-flush"]/*')
 
     def chech_welc_messege(self):
         expect(self.welc_messege).to_have_text("Welcome admin")
+
+    def count_items(self, locator: str):
+        return self.page.locator(locator).count()
+    
+    def check_number_of_menu_elem(self):
+        expect(self.main_menu).to_have_count(5)
+
+
